4d8516c19c77bbd50e8629a55b353b84df317c6b
Anthony Blount <anthony00.blount00@gmail.com>
2022-07-17 23:54:54 +0900

    enemies move and attack on end turn

--- Assets/Scripts/KnightAgent.cs
+++ Assets/Scripts/KnightAgent.cs
@@ -7,7 +7,7 @@ public class KnightAgent : BaseGameAgent
     // Start is called before the first frame update
     void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
